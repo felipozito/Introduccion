5bf38929aef5d2a17d07596e2b811a5e99087a81
Felipe Asimbaya <felipozito@hotmail.com>
2023-04-13 21:32:59 -0500

    Funcion Crear Blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView,CreateBlogView
 
 app_name='blog'
 
 urlpatterns=[
-    path('',BlogListView.as_view(),name='home')
+    path('',BlogListView.as_view(),name='home'),
+    path('create/',CreateBlogView.as_view(),name='create')
     
 ]
